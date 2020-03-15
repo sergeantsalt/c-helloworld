d65c33bf2fc1dab6f7983cd8e38c40612e260e1b
Alex Nelson <sergeantsalt@gmail.com>
2020-03-15 18:25:56 -0400

    try linking gnu scientific library

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 # P=helloworld
 # OBJECTS=helloworld.c square.c shape.c
 CFLAGS = -g -Wall --std=c99
-LDLIBS=
+LDLIBS=`pkg-config --libs gsl`
 CC=gcc
 # $(P): $(OBJECTS)
 
 helloworld : main.o shape.o square.o
-	$(CC) -o helloworld.out main.o shape.o square.o
+	$(CC) -o helloworld.out main.o shape.o square.o $(LDLIBS)
 
 main : main.c shape.c square.c
 	$(CC) -c main.c shape.c square.c
--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include "util.h"
 #include "square.h"
-// #include <gsl/gsl_cdf.h>
+#include <gsl/gsl_cdf.h>
 // #include "shape.h"
 
 int main()
 {
-   // double bottom_tail = gsl_cdf_gaussian_P(-1.96, 1);
-   // printf("Area between [-1.96, 1.96]: %g\n", 1-2*bottom_tail);
-   // return 0;
+   double bottom_tail = gsl_cdf_gaussian_P(-1.96, 1);
+   printf("Area between [-1.96, 1.96]: %g\n", 1-2*bottom_tail);
+   return 0;
 
    Square *s = Square__create(5, 10, 20, 20);
    Shape *shape = Square__to_shape(s);
