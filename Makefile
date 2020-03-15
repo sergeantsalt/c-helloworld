25f41bd37c364fd47ad2a4f0636f9a327ad91aa7
Alex Nelson <sergeantsalt@gmail.com>
2020-03-15 18:10:14 -0400

    added makefile

---
+++ Makefile
@@ -0,0 +1,15 @@
+# P=helloworld
+# OBJECTS=helloworld.c square.c shape.c
+CFLAGS = -g -Wall --std=c99
+LDLIBS=
+CC=gcc
+# $(P): $(OBJECTS)
+
+helloworld : main.o shape.o square.o
+	$(CC) -o helloworld main.o shape.o square.o
+
+main : main.c shape.c square.c
+	$(CC) -c main.c shape.c square.c
+
+clean :
+	rm helloworld *.o
--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include "util.h"
 #include "square.h"
+// #include <gsl/gsl_cdf.h>
 // #include "shape.h"
 
 int main()
 {
+   // double bottom_tail = gsl_cdf_gaussian_P(-1.96, 1);
+   // printf("Area between [-1.96, 1.96]: %g\n", 1-2*bottom_tail);
+   // return 0;
+
    Square *s = Square__create(5, 10, 20, 20);
    Shape *shape = Square__to_shape(s);
    ShapeType *type = Shape__type(shape);
